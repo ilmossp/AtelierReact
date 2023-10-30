3f4f15b83162764415b42c30085616f114597f46
ilmossp <ilyamodeni@gmail.com>
2023-10-31 00:09:06 +0100

    first commit

---
+++ src/utils/api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { Car } from './types'
+
+axios.defaults.url = "http://localhost:8000"
+
+
+export async function getCars(){
+	const {data} = await axios.get("/cars")
+	return data
+}
+
+export async function addCar(body:Car){
+	const {data} = await axios.post("/cars",body)
+	return data
+}
+
+export async function editCar({id,car}:{id:number,car:Car}){
+	const {data} = await axios.patch(`/cars/${id}`,car)
+	return data
+}
+
+export async function deleteCar(id:number){
+	const {data} = await axios.delete(`/cars/${id}`)
+	return data
+}
